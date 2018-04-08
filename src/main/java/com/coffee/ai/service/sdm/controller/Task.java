a7cad9dd54bb7f0e6af8f13d36fa5115a8ca0bd1
coffee-qm <coffee.qm@gmail.com>
2018-04-08 08:17:45 +0800

    basic
    basic

--- src/main/java/com/coffee/ai/service/sdm/controller/Task.java
+++ src/main/java/com/coffee/ai/service/sdm/controller/Task.java
@@ -35,7 +35,6 @@ public class Task {
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public void add(@ModelAttribute final TaskMo mo) {
-		System.out.println(mo);
 		svc.add(mo);
 	}
 
